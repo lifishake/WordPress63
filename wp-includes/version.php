a915074b882ba8adab2b7d9f67eb3cfbe9c808c8
lifishake <lifishake@gmail.com>
2025-03-14 09:33:52 +0800

    删除 post_trackback_meta_box

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-delta-250314';
+$wp_version = '6.8-delta-2503140923';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
